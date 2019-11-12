0a12e10677eb1c44751b8faa0d43eacf2b4cec8d
JoshHayden <jhayden@ithaca.edu>
2019-11-12 14:12:18 -0500

    Base files

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from app.config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+mail = Mail(app)
+bootstrap = Bootstrap(app)
+
+
+
+from app import routes, models, errors
