c31ba472c9f9cc2b65d6726faa9fd8239138f111
Vera Crabtree <vcrabtree99@gmail.com>
2019-12-03 17:32:44 -0500

    Minor Changes

--- app/models.py
+++ app/models.py
@@ -15,12 +15,12 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
 
 class UserToBand(db.Model):
     __tablename__ = 'UserToBand'
@@ -92,7 +92,6 @@ class Event(db.Model):
         self.time = time
         self.bandID = bandID
         self.porchID = porchID
-
 
 
 @login.user_loader
