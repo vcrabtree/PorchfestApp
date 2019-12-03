296f9f6139d171397394b77009760fad56fa3282
JoshHayden <jhayden@ithaca.edu>
2019-12-03 17:24:59 -0500

    Added specific band pages and made the index of bands link to individual pages

--- app/routes.py
+++ app/routes.py
@@ -94,4 +94,16 @@ def reset_db():
     return render_template('index.html', title='Home')
 
 
+@app.route('/artist/<name>')
+def artist(name):
+
+        band = db.session.query(Band).filter_by(name=name).first()
+        events = db.session.query(Event).join(Band, Band.id == Event.bandID).filter(Band.name == name).all()
+        porches = db.session.query(Porch).join(Event, Event.porchID == Porch.id).join(Band, Band.id == Event.bandID).filter(Band.name == name).all()
+        print(porches)
+        print(events)
+
+        return render_template('artistPage.html', title=name, band=band, events=events, porches=porches)
+
+
 
